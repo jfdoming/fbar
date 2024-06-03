80b59fb60482a7690b33f252f570276318882ccc
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2024-06-03 10:05:42 -0400

    Fix REPL

--- repl.js
+++ repl.js
@@ -5,7 +5,7 @@ import api, { q, runQuery } from "@actual-app/api";
 import { setup, teardown } from "./api.js";
 import main from "./main.js";
 import options from "./args.js";
-import ExchangeRateList from "./ExchangeRateList.js";
+import ForeignExchange from "./ForeignExchange.js";
 import * as utils from "./utils.js";
 
 await setup();
@@ -17,9 +17,7 @@ started.context.runQuery = async (query) => (await runQuery(query)).data;
 started.context.rq = started.context.runQuery;
 started.context.pull = (props = {}) =>
   main({ budgetYear: options["budget-year"].promptDefault, ...props });
-started.context.rates = new ExchangeRateList(
-  utils.endOfYear(utils.previousYear())
-);
+started.context.fx = new ForeignExchange(utils.endOfYear(utils.previousYear()));
 Object.keys(utils).forEach((util) => {
   started.context[util] = utils[util];
 });
